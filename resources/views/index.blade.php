126be3dbafb68e5d9fb5d3c0f2c2bae2f1f3dc07
ziel <zielalfinoakbar@gmail.com>
2022-01-27 11:32:41 +0700

    animated sidebar phone ver

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,27 +27,86 @@
         }
     </style>
 </head>
-<body class="w-full bg-red-100" style="height: 200vh">
-    <div id="nav" class="fixed h-20 w-full bg-gray-800 flex justify-center items-center">
-        <button class="absolute left-6 h-8 w-8 rounded-full">
-            <span class="text-red-100 font-bold text-2xl flex justify-center"><i class="fas fa-bars"></i></span>
+<body class="w-full bg-blue-100" style="height: 150vh">
+    <div id="nav" class="fixed h-20 w-full bg-blue-400 flex justify-center items-center">
+        <button id="openSidebar" class="absolute left-6 h-8 w-8 rounded-full">
+            <span class="text-blue-50 font-bold text-2xl flex justify-center"><i class="fas fa-bars"></i></span>
         </button>
-        <span id="ltext" class="text-red-100 text-2xl">Git-Laundry</span>
+        <span id="ltext" class="text-blue-50 text-2xl">Git-Laundry</span>
     </div>
 
     {{-- phone sidebar --}}
-    <div class="fixed w-full h-full bg-black bg-opacity-50">
-        <div id="sidebar" class="w-64 h-full bg-blue-50 fixed z-50 flex flex-col justify-between">
-            <div class="flex items-center p-4 bg-green-200">
+    <div id="sidebar" class="fixed w-full h-full duration-500 transform -translate-x-full">
+        <div id="bodySidebar" class="w-64 h-full bg-blue-50 fixed z-50 flex flex-col justify-between text-gray-600">
+
+            {{-- sidebar header --}}
+            <div class="flex items-center p-4 h-20 bg-gray-700 text-gray-200">
                 <div class="h-12 w-12 bg-red-400 rounded-full mr-2"></div>
-                <span class="text-lg font-bold text-gray-600">Admin</span>
-                <button class="absolute right-4 text-3xl text-gray-600 h-8 w-8 flex justify-center items-center rounded-full"><i class="fas fa-times"></i></button>
+                <span class="text-lg font-bold">Admin</span>
+                <button id="closeSidebar" class="absolute right-4 text-3xl h-8 w-8 flex justify-center items-center rounded-full duration-700 transform -translate-x-8 rotate-90"><i class="fas fa-times"></i></button>
             </div>
-            <div></div>
-            <div class="flex items-center p-4 bg-yellow-100 bottom-0">
-                <span class="font-semibold text-gray-600"><i class="fas fa-sign-out-alt"></i> Logout</span>
+
+            {{-- hyperlinnk list --}}
+            <div class="h-3/4 flex flex-col">
+
+                {{-- first category --}}
+                <div class="mb-4">
+                    <span class="font-bold text-xl ml-2">Category 1</span>
+                    <a class="bg-blue-100 rounded-xl m-2 p-4 flex hover:bg-blue-200 duration-100">
+                        <div class="w-8"><i class="fas fa-plus"></i></div>
+                        <span class="font-semibold">Tambahkan Outlet</span>
+                    </a>
+    
+                    <a class="bg-blue-100 rounded-xl m-2 p-4 flex hover:bg-blue-200 duration-100">
+                        <div class="w-8"><i class="fas fa-user-plus"></i></div>
+                        <span class="font-semibold">Tambahkan Member</span>
+                    </a>
+    
+                    <a class="bg-blue-100 rounded-xl m-2 p-4 flex hover:bg-blue-200 duration-100">
+                        <div class="w-8"><i class="fas fa-box"></i></div>
+                        <span class="font-semibold">Tambahkan Paket</span>
+                    </a>
+                </div>
+
+                {{-- second category --}}
+                <div class="mb-4">
+                    <span class="font-bold text-xl ml-2">Category 2</span>
+                    <a class="bg-blue-100 rounded-xl m-2 p-4 flex hover:bg-blue-200 duration-100">
+                        <div class="w-8"><i class="fas fa-money-check-alt"></i></div>
+                        <span class="font-semibold">Transaction</span>
+                    </a>
+                </div>
+
+            </div>
+
+            {{-- sidebar footer --}}
+            <div class="flex items-center p-4 bg-gray-700 text-gray-200 bottom-0">
+                <span class="font-semibold"><i class="fas fa-sign-out-alt"></i> Logout</span>
             </div>
         </div>
     </div>
+
+
+
+    {{-- main content --}}
+    <div class="pt-24 p-4">
+        <div class="font-bold text-3xl w-full h-64 flex justify-center items-center rounded-xl bg-blue-50 text-gray-600">Hemlo World</div>
+    </div>
+
+
+
+    
+    <script>
+        document.getElementById("openSidebar").onclick = function() {
+          document.getElementById("sidebar").classList.toggle("-translate-x-full");
+          document.getElementById("closeSidebar").classList.toggle("rotate-90");
+          document.getElementById("closeSidebar").classList.toggle("-translate-x-8");
+        //   document.getElementById("body").classList.toggle("overflow-y-hidden");
+          }
+        document.getElementById("closeSidebar").onclick = function() {
+          document.getElementById("sidebar").classList.toggle("-translate-x-full");
+          document.getElementById("closeSidebar").classList.toggle("-translate-x-8");
+          }
+    </script>
 </body>
 </html>
