ad2329e25a9302f0790229269ebe6ecf23248154
ziel <zielalfinoakbar@gmail.com>
2022-01-27 12:07:27 +0700

    custom footbar phone ver

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,7 +27,7 @@
         }
     </style>
 </head>
-<body class="w-full bg-blue-100" style="height: 150vh">
+<body id="body" class="w-full bg-blue-100">
     <div id="nav" class="fixed h-20 w-full bg-blue-400 flex justify-center items-center">
         <button id="openSidebar" class="absolute left-6 h-8 w-8 rounded-full">
             <span class="text-blue-50 font-bold text-2xl flex justify-center"><i class="fas fa-bars"></i></span>
@@ -89,23 +89,54 @@
 
 
     {{-- main content --}}
-    <div class="pt-24 p-4">
+    <div class="pt-24 p-4"  style="height: 100vh">
         <div class="font-bold text-3xl w-full h-64 flex justify-center items-center rounded-xl bg-blue-50 text-gray-600">Hemlo World</div>
     </div>
 
 
 
-    
+    {{-- footer --}}
+    <div class="h-96 bottom-0 bg-gray-700 text-gray-200 flex flex-col">
+        <div class="h-40 flex justify-center items-center flex-col p-8 text-center">
+            <span class="font-bold">About Us</span>
+            ipsum dolor sit amet consectetur adipisicing elit. Autem minima, voluptatum, soluta, distinctio numquam cumque accusantium est cupiditate laboriosam omnis quisquam iste accusamus excepturi quae.
+        </div>
+        <div class="flex flex-col h-48">
+            <div class="h-24 w-full flex flex-col justify-center items-center p-8">
+                <span class="font-bold">Adress</span>
+                <div class="w-72">Lorem ipsum dolor sit amet, consectetur adipisicing elit temporibus</div>
+            </div>
+            <div class="h-24 w-full flex flex-col justify-center items-center p-8">
+                <span class="font-bold">Contact Us</span>
+                <div class="flex">
+                    <a class="h-12 w-12 rounded-full bg-gray-200 mx-1 flex justify-center items-center text-2xl text-gray-700 hover:bg-gray-500 duration-100">
+                        <i class="fas fa-map-marked-alt"></i>
+                    </a>
+                    <a class="h-12 w-12 rounded-full bg-gray-200 mx-1 flex justify-center items-center text-2xl text-gray-700 hover:bg-gray-500 duration-100">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a class="h-12 w-12 rounded-full bg-gray-200 mx-1 flex justify-center items-center text-2xl text-gray-700 hover:bg-gray-500 duration-100">
+                        <i class="fab fa-whatsapp"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="h-8 w-full flex justify-center items-center">
+            <span class="text-gray-500 italic">&copy; agerr.studio 2022</span>
+        </div>
+    </div>
+
     <script>
         document.getElementById("openSidebar").onclick = function() {
           document.getElementById("sidebar").classList.toggle("-translate-x-full");
           document.getElementById("closeSidebar").classList.toggle("rotate-90");
           document.getElementById("closeSidebar").classList.toggle("-translate-x-8");
-        //   document.getElementById("body").classList.toggle("overflow-y-hidden");
+          document.getElementById("body").classList.toggle("overflow-y-hidden");
           }
         document.getElementById("closeSidebar").onclick = function() {
           document.getElementById("sidebar").classList.toggle("-translate-x-full");
           document.getElementById("closeSidebar").classList.toggle("-translate-x-8");
+          document.getElementById("body").classList.toggle("overflow-y-hidden");
           }
     </script>
 </body>
