75b7a297e888b1ead662f8dff210019c43c4f528
ziel <zielalfinoakbar@gmail.com>
2022-01-27 10:22:43 +0700

    custom sidebar phone ver

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,5 +34,20 @@
         </button>
         <span id="ltext" class="text-red-100 text-2xl">Git-Laundry</span>
     </div>
+
+    {{-- phone sidebar --}}
+    <div class="fixed w-full h-full bg-black bg-opacity-50">
+        <div id="sidebar" class="w-64 h-full bg-blue-50 fixed z-50 flex flex-col justify-between">
+            <div class="flex items-center p-4 bg-green-200">
+                <div class="h-12 w-12 bg-red-400 rounded-full mr-2"></div>
+                <span class="text-lg font-bold text-gray-600">Admin</span>
+                <button class="absolute right-4 text-3xl text-gray-600 h-8 w-8 flex justify-center items-center rounded-full"><i class="fas fa-times"></i></button>
+            </div>
+            <div></div>
+            <div class="flex items-center p-4 bg-yellow-100 bottom-0">
+                <span class="font-semibold text-gray-600"><i class="fas fa-sign-out-alt"></i> Logout</span>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
